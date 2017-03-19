ec86ea57e8850bb9fdb0b6c6aae65e7a50b93273
Joseph Naegele <jnaegele6@gatech.edu>
2017-03-19 14:51:16 -0400

    add basic test script for Biowulf

---
+++ sleeptest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sleep 10
+echo goodnight all
+sleep 10
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+NOBATCH=1 ./fsl_sub -v -N local -l ./local -T 0 -q unlimited -a amd64 -p 0 -M foo@example.com -j 0 git log -n 1 --pretty=email
+./fsl_sub -v -N helloworld -l ./helloworld -T 1 -q nimh echo hello world
+FSL_QUEUE=nimh ./fsl_sub -v -N omp -l ./openmp -s openmp,4 echo "\$OMP_NUM_THREADS"
+FSL_MEM=3 ./fsl_sub -v -N sleeptest -l ./sleeptest -T 1 -t sleeptest.sh
